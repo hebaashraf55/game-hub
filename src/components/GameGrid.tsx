79ff253f90339d563763ad2b4ba363fc06c34455
heba <hebaashraf81@gmail.com>
2024-08-18 16:10:05 +0300

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -20,7 +20,7 @@ const GameGrid = () => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10"
-        spacing={10}
+        spacing={3}
       >
         {data.map((game) => (
 
