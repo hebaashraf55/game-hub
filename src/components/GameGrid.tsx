cd875297f779027769c8af36e7147a2d1ac0bdf3
heba <hebaashraf81@gmail.com>
2024-09-05 00:53:03 +0300

    Refactor Game Gride

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,20 +16,23 @@ const GameGrid = ({ gameQuery} : Props ) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>
+
   return (
-    <>
-      {isLoading &&
+    
+      
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10"
+        spacing={6}
+      >
+        {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
             <GameCardSkeleton  />
           </GameCardContainer>
         ))}
-      {error && <Text> {error} </Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4  }}
-        padding="10"
-        spacing={6}
-      >
+      
         {data.map((game) => (
 
           <GameCardContainer key={game.id} >
@@ -38,7 +41,7 @@ const GameGrid = ({ gameQuery} : Props ) => {
 
         ))}
       </SimpleGrid>
-    </>
+    
   );
 };
 
